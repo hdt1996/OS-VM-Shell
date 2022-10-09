dd9ae4ecb6fd6ba5263be2d7a3c798f48bfc0309
hdt1996 <h.duong0818@gmail.com>
2022-10-09 01:07:47 -0700

    arduino update

--- ubu64/Setup/PRGM/arduino_setup.sh
+++ ubu64/Setup/PRGM/arduino_setup.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+USER=$(echo "$USER")
+#sudo snap install arduino 
+#Source files in D drive for arduino (2.0+) 
+sudo usermod -a -G dialout "$USER" #Serial data uses dialout group
 
-sudo snap install arduino
+sudo chmod 660 /dev/ttyS0 #Virtual box, we enable Serial Port 1, assign PORT address COM3
+			  #or whatever COM port arduino is connected to
+			  #Set host device with connect to Pipe(should be true as default)
+			  #USB, WE NEED USB 2.0/3.0 at LEAST. ADD ARDUINO TO FILTER
+#Assuming source file is sent to myshare
+
+sudo su
+cp -r /home/$USER/myshare/arduino /usr/bin/arduino
+chown $USER:$USER /usr/bin/arduino
+
+read -p "Proceed to make desktop shortcut using Admin's Create_Shortcut" x
+
 

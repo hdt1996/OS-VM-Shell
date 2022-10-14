a1026c47941019d5dd48a475ad83166d753f3a91
hdt1996 <h.duong0818@gmail.com>
2022-10-14 00:48:07 -0700

    Sync

--- ubu64/Setup/PRGM/arduino_setup.sh
+++ ubu64/Setup/PRGM/arduino_setup.sh
@@ -11,9 +11,14 @@ sudo chmod 660 /dev/ttyS0 #Virtual box, we enable Serial Port 1, assign PORT add
 #Assuming source file is sent to myshare
 
 sudo su
-cp -r /home/$USER/myshare/arduino /usr/bin/arduino
-chown $USER:$USER /usr/bin/arduino
-
+#cp -r /home/$USER/myshare/arduino /usr/bin/arduino
+#chown $USER:$USER /usr/bin/arduino
+wget https://downloads.arduino.cc/arduino-1.8.13-linux64.tar.xz -O /tmp/arduino_install.tar.xz;
+cd /tmp
+tar xvf ./arduino_install.tar.xz
+sudo mv ./arduino-1.8.13 /opt/
+cd /opt/arduino-1.8.13
+sudo ./install.sh
 read -p "Proceed to make desktop shortcut using Admin's Create_Shortcut" x
 
 
